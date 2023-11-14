1001bc2dd7d356b32b32a7f81ec3b28ee0e6587d
Romain KOENIG <romain@kromatic.fr>
2023-11-14 12:27:57 +0100

    Force a refresh

--- _data/sites/sites.js
+++ _data/sites/sites.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "Sites développés par l'équipe",
 	options: {
 		runs: 1,
-		frequency: 60 * 23, // (in minutes)
+		frequency: 1, // (in minutes)
 	},
 	urls: [
 		"https://villaontherocks.fr",
