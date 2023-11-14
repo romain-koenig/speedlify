e46050e642cfbb8244edde1b058b0bb16c97f668
Romain KOENIG <romain@kromatic.fr>
2023-11-14 15:14:10 +0100

    Some more sites

--- _data/sites/clients.js
+++ _data/sites/clients.js
@@ -3,16 +3,23 @@ module.exports = {
 	description: "Sites de nos clients",
 	options: {
 		runs: 1,
-		frequency: 1, // (in minutes)
+		frequency: 60, // (in minutes)
 	},
 	urls: [
 		"https://www.agence-environnement-sante.fr/",
 		"https://cotiere-transition.fr/",
 		"https://grainesdesol-formation.fr",
 		"https://hopandup.fr",
-		"https://rvkom.fr",
+		"https://www.rvkom.fr",
 		"https://annuaire.grainesdesol.fr",
 		"https://ipamac.fr",
 		"https://sophro-nimes.fr",
+		"https://lesoinensoi.fr",
+		"https://vertasouhait.fr",
+		"https://senproprete.fr",
+		"https://missorigone.fr",
+		"https://gymclubmulatiere.fr",
+		"https://iletaitunefois-patisserie.fr",
+
 	]
 };
